ff3382cd2a150b15825770a142de57bb1f3b8769
marzhin <pereeesvet@gmail.com>
2026-06-16 16:40:34 +0700

    add list task endpoint

--- app/api/v1/tasks.py
+++ app/api/v1/tasks.py
@@ -12,3 +12,8 @@ router = APIRouter(prefix='/tasks', tags=['Tasks'])
 async def create_task(task_data: TaskCreate, task_service: TaskService = Depends(get_task_service), current_user: User = Depends(get_current_user)):
     task = await task_service.create_task(task_data, current_user)
     return task
+
+@router.get('')
+async def get_tasks(task_service: TaskService = Depends(get_task_service), current_user: User = Depends(get_current_user)):
+    tasks = await task_service.list_tasks(current_user=current_user)
+    return tasks
